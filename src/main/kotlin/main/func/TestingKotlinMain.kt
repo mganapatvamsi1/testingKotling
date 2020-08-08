bab783221308e787c9c59db280f4ad491993a129
mganapatvamsi1 <ganapathiraju.vamsi@gmail.com>
2020-08-08 07:24:53 -0500

    null safe operations
    null safe operations

--- src/main/kotlin/main/func/TestingKotlinMain.kt
+++ src/main/kotlin/main/func/TestingKotlinMain.kt
@@ -5,6 +5,7 @@ import org.springframework.boot.runApplication
 @org.springframework.boot.autoconfigure.SpringBootApplication
 class TestingKotlinMain {
     fun main(args: Array<String>) {
+//        runApplication<TestingKotlinMain>()
         runApplication<TestingKotlinMain>(*args)
     }
 }
